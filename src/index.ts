5877badd799c4d166ac85c267e67f64f9287a26c
StaticKit <support@statickit.com>
2020-01-08 12:29:47 -0600

    Deployment d4402056-02f3-41ff-8e6a-03b18c798d01

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { SendNotification, FunctionOptions } from './types';
+import { StaticKit, createClient } from '@statickit/core';
+
 export { StaticKit, createClient } from '@statickit/core';
+
+export function sendEmail(
+  client: StaticKit,
+  args: SendNotification.Args,
+  opts: FunctionOptions = {}
+): Promise<SendNotification.Response> {
+  return client.invoke('sendEmail', args, opts);
+};
