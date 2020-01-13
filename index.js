9505a3e86ad56a0e25b4eeb57da12902b0125b13
StaticKit <support@statickit.com>
2020-01-13 14:37:09 -0600

    Define functions for 93815cd4-84f1-42f1-b518-6808862883b1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// empty
+function sendEmail(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendEmail', args, opts);
+};
+exports.sendEmail = sendEmail;
+
