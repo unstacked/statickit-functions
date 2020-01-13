cfd25ccb5a157b435cc10358bd08a7437b11f3f2
StaticKit <support@statickit.com>
2020-01-13 12:47:54 -0600

    Declare types for 6df80b1d-9986-455e-a2da-661198cfe15c

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,5 @@
-// empty
+import { SendNotification } from './types';
+import { StaticKit } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function sendSupportEmail(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
