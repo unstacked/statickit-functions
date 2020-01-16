151125e75f6a1cfba814ee45f3ac593f63fd35c8
StaticKit <support@statickit.com>
2020-01-15 20:12:12 -0600

    Define functions for c665ad0c-9029-4aef-ad1e-51b3f70c80ec

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function sendSupportEmail(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendSupportEmail', args, opts);
+};
+exports.sendSupportEmail = sendSupportEmail;
+
+exports.createClient = core.createClient;
+
