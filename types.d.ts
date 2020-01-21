acea95dedba0f53538f292593d822a48ea402392
Derrick Reimer <derrickreimer@gmail.com>
2020-01-21 10:14:24 -0600

    Add Mailchimp AddOrUpdateListMember types

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,7 @@ import {
   FunctionArgs,
   FunctionSuccess,
   Failure
-} from '@statickit/core/functions';
+} from "@statickit/core/functions";
 
 export namespace SendNotification {
   export interface Args extends FunctionArgs {
@@ -13,4 +13,19 @@ export namespace SendNotification {
 
   export interface Success extends FunctionSuccess {}
   export type Response = Success | Failure;
+}
+
+export namespace Mailchimp {
+  export namespace AddOrUpdateListMember {
+    export interface Args extends FunctionArgs {
+      emailAddress: string;
+      mergeFields?: object;
+    }
+
+    export interface Success extends FunctionSuccess {
+      id: string;
+    }
+
+    export type Response = Success | Failure;
+  }
 }
