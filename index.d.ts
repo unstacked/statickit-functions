e38e6657696fb1a4abd86f44733894a605222b08
StaticKit <support@statickit.com>
2020-01-21 13:25:52 -0600

    Declare types for 05562211-01b7-4b68-9791-27cd430ac43f

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-// empty
+import { SendNotification, Mailchimp } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function addToMailchimp(client: StaticKit, args: Mailchimp.CreateEvent.Args, opts?: FunctionOptions): Promise<Mailchimp.CreateEvent.Response>;
+export declare function sendEmail(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function createClient(config: Config): StaticKit;
+
