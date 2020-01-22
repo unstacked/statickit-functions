a5e196fa09de93d4b95888eb0ef146abe6ae9d3a
StaticKit <support@statickit.com>
2020-01-22 10:57:02 -0600

    Declare types for c22edb5e-4627-49d1-822e-a5440ba4090e

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,9 @@
-// empty
+import { SendNotification, Stripe, Mailchimp } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function addToMailchimp(client: StaticKit, args: Mailchimp.CreateEvent.Args, opts?: FunctionOptions): Promise<Mailchimp.CreateEvent.Response>;
+export declare function createCustomer(client: StaticKit, args: Stripe.CreateCustomer.Args, opts?: FunctionOptions): Promise<Stripe.CreateCustomer.Response>;
+export declare function sendEmail(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function createClient(config: Config): StaticKit;
+
