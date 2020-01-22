efee2700952318229cd9d1385b1984bf805503ab
StaticKit <support@statickit.com>
2020-01-22 17:41:38 -0600

    Define functions for 7ede91fe-b365-4584-82e9-dee2ed5846ed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// empty
+var core = require('@statickit/core');
+
+function addToMailchimp(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('addToMailchimp', args, opts);
+};
+exports.addToMailchimp = addToMailchimp;
+
+function createCharge(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createCharge', args, opts);
+};
+exports.createCharge = createCharge;
+
+function createCustomer(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createCustomer', args, opts);
+};
+exports.createCustomer = createCustomer;
+
+function createSubscription(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createSubscription', args, opts);
+};
+exports.createSubscription = createSubscription;
+
+function sendEmail(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendEmail', args, opts);
+};
+exports.sendEmail = sendEmail;
+
+exports.createClient = core.createClient;
+
