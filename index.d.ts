815af0472c86208e07d4b6a2ce4ee1c4f201c1ee
StaticKit <support@statickit.com>
2020-01-22 17:28:03 -0600

    Declare types for d8a48917-98c7-4dbf-914c-256b6c0e7a85

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,11 @@
-// empty
+import { SendNotification, Stripe, Mailchimp } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function addToMailchimp(client: StaticKit, args: Mailchimp.CreateEvent.Args, opts?: FunctionOptions): Promise<Mailchimp.CreateEvent.Response>;
+export declare function createCharge(client: StaticKit, args: Stripe.CreateCharge.Args, opts?: FunctionOptions): Promise<Stripe.CreateCharge.Response>;
+export declare function createCustomer(client: StaticKit, args: Stripe.CreateCustomer.Args, opts?: FunctionOptions): Promise<Stripe.CreateCustomer.Response>;
+export declare function createSubscription(client: StaticKit, args: Stripe.CreateSubscription.Args, opts?: FunctionOptions): Promise<Stripe.CreateSubscription.Response>;
+export declare function sendEmail(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function createClient(config: Config): StaticKit;
+
