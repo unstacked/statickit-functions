583aa53a48bb5462eb69d7dba83261715c40c2cd
StaticKit <support@statickit.com>
2020-01-30 16:25:11 -0500

    Define functions for e270cc41-fd3d-4868-804f-60c1fea00057

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function createCharge(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createCharge', args, opts);
+};
+exports.createCharge = createCharge;
+
+exports.createClient = core.createClient;
+
