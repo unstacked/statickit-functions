73092216fefad32147ee9efad112c0e454192935
StaticKit <support@statickit.com>
2020-01-30 11:01:41 -0500

    Define functions for 27497d05-8609-4af9-90f4-daad4d9d4efe

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// empty
+var core = require('@statickit/core');
+
+function addToMailchimp(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('addToMailchimp', args, opts);
+};
+exports.addToMailchimp = addToMailchimp;
+
+function helloWorld(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('helloWorld', args, opts);
+};
+exports.helloWorld = helloWorld;
+
+exports.createClient = core.createClient;
+
