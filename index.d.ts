47e536bd7370b40badf6f76364913d5dee586683
StaticKit <support@statickit.com>
2020-01-31 11:06:26 -0600

    Declare types for 30f0390a-e66a-4077-bc9a-0f59441ec245

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,9 @@
-// empty
+import { Stripe } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function createCharge(client: StaticKit, args: Stripe.CreateCharge.Args, opts?: FunctionOptions): Promise<Stripe.CreateCharge.Response>;
+export declare function createCustomer(client: StaticKit, args: Stripe.CreateCustomer.Args, opts?: FunctionOptions): Promise<Stripe.CreateCustomer.Response>;
+export declare function createSubscription(client: StaticKit, args: Stripe.CreateSubscription.Args, opts?: FunctionOptions): Promise<Stripe.CreateSubscription.Response>;
+export declare function createClient(config: Config): StaticKit;
+
