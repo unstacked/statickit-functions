21d566e0cdc0f44b0bd4f0a71d26914d6ca896e0
StaticKit <support@statickit.com>
2020-02-03 15:49:08 -0600

    Define functions for 08c4a9d1-5fe0-4193-ae3b-52df1ea068a2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function sendContactMessage(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendContactMessage', args, opts);
+};
+exports.sendContactMessage = sendContactMessage;
+
+exports.createClient = core.createClient;
+
