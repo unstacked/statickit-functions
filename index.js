472396c181325b8793475e5613996d053869cbbe
StaticKit <support@statickit.com>
2020-02-04 01:57:41 -0600

    Define functions for 7843d44e-0bfa-4ec4-aa69-dc7c69a862f1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function addToConvertKit(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('addToConvertKit', args, opts);
+};
+exports.addToConvertKit = addToConvertKit;
+
+exports.createClient = core.createClient;
+
