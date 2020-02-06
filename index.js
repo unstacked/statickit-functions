499521411970ea65549ada604b291e9a44297b7b
StaticKit <support@statickit.com>
2020-02-06 01:37:44 -0600

    Define functions for 21bd2746-d607-4237-9dd7-374feab406d3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// empty
+var core = require('@statickit/core');
+
+function addToConvertKit(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('addToConvertKit', args, opts);
+};
+exports.addToConvertKit = addToConvertKit;
+
+function addToMailchimp(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('addToMailchimp', args, opts);
+};
+exports.addToMailchimp = addToMailchimp;
+
+exports.createClient = core.createClient;
+
