f4f798e69a08d18ae93e40d022ccddb150ac1242
StaticKit <support@statickit.com>
2020-02-06 01:37:45 -0600

    Declare types for 21bd2746-d607-4237-9dd7-374feab406d3

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-// empty
+import { Mailchimp, ConvertKit } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function addToConvertKit(client: StaticKit, args: ConvertKit.ApplyTags.Args, opts?: FunctionOptions): Promise<ConvertKit.ApplyTags.Response>;
+export declare function addToMailchimp(client: StaticKit, args: Mailchimp.AddOrUpdateListMember.Args, opts?: FunctionOptions): Promise<Mailchimp.AddOrUpdateListMember.Response>;
+export declare function createClient(config: Config): StaticKit;
+
