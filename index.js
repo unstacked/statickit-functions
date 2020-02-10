de7277c6bdc8bd26c3885326594f0e5ff48fb02f
StaticKit <support@statickit.com>
2020-02-10 13:05:28 -0600

    Define functions for d1345cc3-9d4e-4bfd-a34d-e24334a0933b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// empty
+var core = require('@statickit/core');
+
+function requestUpgrade(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('requestUpgrade', args, opts);
+};
+exports.requestUpgrade = requestUpgrade;
+
+function sendSupportEmail(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendSupportEmail', args, opts);
+};
+exports.sendSupportEmail = sendSupportEmail;
+
+function signUpSurvey(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('signUpSurvey', args, opts);
+};
+exports.signUpSurvey = signUpSurvey;
+
+exports.createClient = core.createClient;
+
