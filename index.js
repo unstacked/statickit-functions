c1f8aa2db81bfbea24baa2bb9558118f9ed237e0
StaticKit <support@statickit.com>
2020-02-12 08:48:04 -0600

    Define functions for 7bc93029-1a64-4737-aa03-72db86582062

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function notifySales(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('notifySales', args, opts);
+};
+exports.notifySales = notifySales;
+
+exports.createClient = core.createClient;
+
