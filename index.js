46ea31535d7af53c15a30c34071e2871e6b872d0
StaticKit <support@statickit.com>
2020-02-21 00:13:51 -0600

    Define functions for a76579b1-2e72-42bd-bff5-782f3094ae28

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// empty
+var core = require('@statickit/core');
+
+function helloWorld(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('helloWorld', args, opts);
+};
+exports.helloWorld = helloWorld;
+
+function websiteContactForm(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('websiteContactForm', args, opts);
+};
+exports.websiteContactForm = websiteContactForm;
+
+exports.createClient = core.createClient;
+
