b883a14bf3aa933f12dba9df11c875288b266531
StaticKit <support@statickit.com>
2020-03-05 08:21:15 -0600

    Define functions for 8cb50d5d-cdda-4fe0-bce1-5df91cd381de

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// empty
+var core = require('@statickit/core');
+
+function addToMailchimp(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('addToMailchimp', args, opts);
+};
+exports.addToMailchimp = addToMailchimp;
+
+function sendContactEmail(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendContactEmail', args, opts);
+};
+exports.sendContactEmail = sendContactEmail;
+
+exports.createClient = core.createClient;
+
