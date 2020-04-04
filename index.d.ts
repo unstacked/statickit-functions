217ed84aa9c2d9d2e6fa79736d3863913665c27d
StaticKit <support@statickit.com>
2020-04-04 14:14:25 -0500

    Declare types for ddc93ddc-9d0c-4250-880b-4b1a2441ad93

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
-// empty
+import { ConvertKit } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function addToConvertKit(client: StaticKit, args: ConvertKit.ApplyTags.Args, opts?: FunctionOptions): Promise<ConvertKit.ApplyTags.Response>;
+export declare function createClient(config: Config): StaticKit;
+
