3d0fd90f09f0825486c4e269e27dd661e16b4963
StaticKit <support@statickit.com>
2020-05-28 23:41:15 -0500

    Define functions for 20a8abd0-3417-4582-8c30-4bcde70eb553

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function submitContactForm(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('submitContactForm', args, opts);
+};
+exports.submitContactForm = submitContactForm;
+
+exports.createClient = core.createClient;
+
