a3e79ecf37f3075295a40ef7320e3d875bab77c5
StaticKit <support@statickit.com>
2020-05-31 10:54:06 -0500

    Define functions for 9632e97a-ab43-4a60-83af-705ab6875043

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function performMagic(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('performMagic', args, opts);
+};
+exports.performMagic = performMagic;
+
+exports.createClient = core.createClient;
+
