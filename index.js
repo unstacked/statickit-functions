a598e14c3ef765a49aa546e8f5eafcf4a32c2a77
StaticKit <support@statickit.com>
2020-06-26 16:54:20 -0500

    Define functions for 86684f12-11e5-4c60-b5a8-77f225a319c1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function createSubscription(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createSubscription', args, opts);
+};
+exports.createSubscription = createSubscription;
+
+exports.createClient = core.createClient;
+
