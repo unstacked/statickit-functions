e143319544b4818baf5ded6f83dedf0cac301dd8
StaticKit <support@statickit.com>
2020-06-26 18:54:03 -0500

    Define functions for 8b363a89-063b-4643-b04d-1492cab242e8

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// empty
+var core = require('@statickit/core');
+
+function createCustomer(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createCustomer', args, opts);
+};
+exports.createCustomer = createCustomer;
+
+function createSubscription(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createSubscription', args, opts);
+};
+exports.createSubscription = createSubscription;
+
+exports.createClient = core.createClient;
+
