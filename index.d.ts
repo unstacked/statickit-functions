58f978db7fd3c41e3d6f17e68036bf2f3447bf11
StaticKit <support@statickit.com>
2020-06-26 16:54:20 -0500

    Declare types for 86684f12-11e5-4c60-b5a8-77f225a319c1

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
-// empty
+import { Stripe } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function createSubscription(client: StaticKit, args: Stripe.CreateSubscription.Args, opts?: FunctionOptions): Promise<Stripe.CreateSubscription.Response>;
+export declare function createClient(config: Config): StaticKit;
+
