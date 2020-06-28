94b6f218414110bcd1f2646a0f11245b0949b115
StaticKit <support@statickit.com>
2020-06-28 14:54:56 -0500

    Declare types for 7439753a-3780-4626-8805-5c5f9020afef

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,9 @@
-// empty
+import { SendNotification, Stripe } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function createCustomer(client: StaticKit, args: Stripe.CreateCustomer.Args, opts?: FunctionOptions): Promise<Stripe.CreateCustomer.Response>;
+export declare function createSubscription(client: StaticKit, args: Stripe.CreateSubscription.Args, opts?: FunctionOptions): Promise<Stripe.CreateSubscription.Response>;
+export declare function submitContactForm(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function createClient(config: Config): StaticKit;
+
