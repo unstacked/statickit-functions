adea27c07f2c28a1f48957ab1fca5b2e2886fbe5
StaticKit <support@statickit.com>
2020-06-28 14:58:11 -0500

    Define functions for ed6ad091-c8be-4587-8362-290bd36b2200

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// empty
+var core = require('@statickit/core');
+
+function createCustomer(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createCustomer', args, opts);
+};
+exports.createCustomer = createCustomer;
+
+function createSubscription(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createSubscription', args, opts);
+};
+exports.createSubscription = createSubscription;
+
+function submitContactForm(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('submitContactForm', args, opts);
+};
+exports.submitContactForm = submitContactForm;
+
+exports.createClient = core.createClient;
+
