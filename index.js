15d488faf61940d9db2648d6b05e84e091cfa37d
StaticKit <support@statickit.com>
2020-06-29 12:38:00 -0500

    Define functions for 57e77905-6bea-4bec-b12b-0dd5d674e926

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// empty
+var core = require('@statickit/core');
+
+function sendSMS(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendSMS', args, opts);
+};
+exports.sendSMS = sendSMS;
+
+exports.createClient = core.createClient;
+
