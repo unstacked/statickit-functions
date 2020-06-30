b935f85efb4d5f0fc3fd516ff52d6a620030ff40
StaticKit <support@statickit.com>
2020-06-30 10:18:17 -0500

    Define functions for e6023028-5325-48d6-9371-6ee3765871d3

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// empty
+var core = require('@statickit/core');
+
+function createCustomer(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createCustomer', args, opts);
+};
+exports.createCustomer = createCustomer;
+
+function createSubscription(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createSubscription', args, opts);
+};
+exports.createSubscription = createSubscription;
+
+function createTestCustomer(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createTestCustomer', args, opts);
+};
+exports.createTestCustomer = createTestCustomer;
+
+function createTestSubscripion(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createTestSubscripion', args, opts);
+};
+exports.createTestSubscripion = createTestSubscripion;
+
+function createTestSubscription(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('createTestSubscription', args, opts);
+};
+exports.createTestSubscription = createTestSubscription;
+
+function submitContactForm(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('submitContactForm', args, opts);
+};
+exports.submitContactForm = submitContactForm;
+
+exports.createClient = core.createClient;
+
