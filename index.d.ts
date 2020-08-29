d6828382ea0d494525cb73466474726ae1a5aedc
StaticKit <support@statickit.com>
2020-08-29 11:29:34 -0500

    Declare types for 1ebb988c-860f-43ae-bc2e-d27d67f5e205

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-// empty
+import { SendNotification } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function performMagic(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function sendContactEmail(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function createClient(config: Config): StaticKit;
+
