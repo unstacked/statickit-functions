261c6e5ddbcd07d1527f4d0ebf05891666a7d5dd
StaticKit <support@statickit.com>
2020-08-29 11:29:33 -0500

    Define functions for 1ebb988c-860f-43ae-bc2e-d27d67f5e205

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// empty
+var core = require('@statickit/core');
+
+function performMagic(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('performMagic', args, opts);
+};
+exports.performMagic = performMagic;
+
+function sendContactEmail(client, args, opts) {
+  if (opts === void 0) { opts = {}; }
+  return client.invokeFunction('sendContactEmail', args, opts);
+};
+exports.sendContactEmail = sendContactEmail;
+
+exports.createClient = core.createClient;
+
