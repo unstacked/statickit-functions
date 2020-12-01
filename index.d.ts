18ca86b2b8e1de9fe6c5dd27b4b24cb7ec6a7cba
StaticKit <support@statickit.com>
2020-12-01 07:18:44 -0600

    Declare types for ba254a73-bf5c-4526-8878-4ecb4359b792

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-// empty
+import { SendNotification } from './types';
+import { StaticKit, Config } from '@statickit/core';
+import { FunctionOptions } from '@statickit/core/functions';
+
+export declare function helloWorld(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function submitContactForm(client: StaticKit, args: SendNotification.Args, opts?: FunctionOptions): Promise<SendNotification.Response>;
+export declare function createClient(config: Config): StaticKit;
+
